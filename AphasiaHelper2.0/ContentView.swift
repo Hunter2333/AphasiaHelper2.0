f41181e9aa77f3c8f6ac7385f4818474cbe63d38
Hunter2333 <1249881412@qq.com>
2020-12-13 12:53:09 +0800

    preview

--- AphasiaHelper2.0/ContentView.swift
+++ AphasiaHelper2.0/ContentView.swift
@@ -44,156 +44,270 @@ struct ContentView: View {
             Word(id: 3, name: "画画"),
             Word(id: 4, name: "睡觉")
         ]
+
+    
+    
         
     var body: some View {
         
         GeometryReader { geo in
             
-            HStack {
-                // 左侧栏
-                VStack {
+            VStack {
+                
+                // 标题
+                HStack {
                     Spacer()
-                    VStack {
-                        Button(action: {}) {
-                            Image(systemName: "camera").font(.system(size: 40, weight: .regular))
-                        }
-                        .frame(width: 100, height: 100)
-                        Button(action: {}) {
-                            Image(systemName: "mic.fill").font(.system(size: 40, weight: .regular))
-                        }
-                        .frame(width: 100, height: 100)
-                        Button(action: {}) {
-                            Image(systemName: "paintpalette").font(.system(size: 40, weight: .regular))
-                        }
-                        .frame(width: 100, height: 100)
-                    }.padding(.bottom, 30)
+                    Text("选词造句")
+                        .font(.title)
+                        .bold()
+                    Spacer()
                 }
                 .foregroundColor(Color.white)
-                .background(Color.black)
-                .shadow(radius: 10)
-                .frame(width: 100)
+                .background(Color(red: 59/255, green: 142/255, blue: 136/255).frame(width: geo.size.width, height: 60))
+                .frame(width: geo.size.width, height: 60, alignment: .center)
+                
+                
+                // 组成的一句话
+                HStack {
+                    
+                    Spacer()
+                    HStack {
+                        HStack {
+                            Text(self.sentance)
+                                .font(.title2)
+                                .foregroundColor(Color.black)
+                        }
+                        .frame(width: geo.size.width * 0.6 - 70, height: 60, alignment: .leading)
+                        .offset(x: 50)
+                        
+                        Button(action: {
+                            // TODO 智能识别
+                        }){
+                            Image(systemName: "camera").font(.system(size: 28, weight: .bold))
+                        }
+                        .foregroundColor(Color(red: 59/255, green: 142/255, blue: 136/255))
+                        .frame(width: 70, height: 60, alignment: .center)
+                        .offset(x: -12)
+                    }
+                    .background(Color.white.frame(width: geo.size.width * 0.6, height: 60))
+                    .frame(width: geo.size.width * 0.6, height: 60)
+                    .cornerRadius(30)
+                    
+                    
+                    Spacer()
+                    Button(action: {
+                        read(text: self.sentance)
+                    }){
+                        Image(systemName: "speaker.wave.2").font(.system(size: 28, weight: .bold))
+                    }
+                    .padding(15.25)
+                    .foregroundColor(Color(red: 59/255, green: 142/255, blue: 136/255))
+                    .background(Color.white)
+                    .clipShape(Circle())
+                    
+                    
+                    Button(action: {
+                        // TODO
+                        self.sentance = ""
+                    }){
+                        Image(systemName: "clear").font(.system(size: 32, weight: .bold))
+                    }
+                    .padding(13)
+                    .foregroundColor(Color(red: 59/255, green: 142/255, blue: 136/255))
+                    .background(Color.white)
+                    .clipShape(Circle())
+                    
+                    
+                    Spacer()
+                    HStack {
+                        Button(action: {
+                            // TODO 添加词语
+                        }){
+                            Image(systemName: "plus").font(.system(size: 34, weight: .bold))
+                        }
+                        .padding(13)
+                        .foregroundColor(Color(red: 59/255, green: 142/255, blue: 136/255))
+                        .background(Color.white)
+                        .clipShape(Circle())
+                    }
+                    .frame(width: 120, height: 120, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                }
+                .background(Color(red: 59/255, green: 142/255, blue: 136/255).frame(width: geo.size.width, height: 120))
+                .frame(width: geo.size.width, height: 120)
                 
                 
                 // 选词区域
                 VStack {
+                    
+                    Spacer()
+                    
                     HStack {
-                        VStack {
+                        
+                        Spacer()
+                        
+                        // 主语
+                        ZStack {
+                            
+                            RoundedRectangle(cornerRadius: 6)
+                                .fill(Color.white)
+                                .shadow(radius: 2)
+                            
                             VStack {
+                                
                                 HStack {
                                     Text("主语")
-                                        .font(.title2)
+                                        .font(.title3)
                                         .bold()
+                                        .padding(.leading, 15)
                                     Spacer()
                                 }
-                                .frame(alignment: .topLeading)
+                                
                                 ScrollView(.horizontal, showsIndicators: true) {
                                     HStack {
                                         ForEach(subjects, id: \.id) { word in
                                             WordBtnView(word: word, sentance: self.$sentance)
                                         }
                                     }
-                                }
+                                }.padding(.leading, 15)
+
+                                ScrollView(.horizontal, showsIndicators: true) {
+                                    HStack {
+                                        ForEach(subjects, id: \.id) { word in
+                                            WordBtnView(word: word, sentance: self.$sentance)
+                                        }
+                                    }
+                                }.padding(.leading, 15)
                             }
-                            .frame(width: (geo.size.width - 110) * 0.75 - 80, height: (geo.size.height * 0.8 - 40) / 4)
-                            .background(Color.green)
+                        }
+                        
+                        Spacer()
+                        
+                        // 谓语
+                        ZStack {
+                            
+                            RoundedRectangle(cornerRadius: 6)
+                                .fill(Color.white)
+                                .shadow(radius: 2)
+                            
                             VStack {
+                                
                                 HStack {
                                     Text("谓语")
-                                        .font(.title2)
+                                        .font(.title3)
                                         .bold()
+                                        .padding(.leading, 15)
                                     Spacer()
                                 }
-                                .frame(alignment: .topLeading)
+                                
                                 ScrollView(.horizontal, showsIndicators: true) {
                                     HStack {
                                         ForEach(predicates, id: \.id) { word in
                                             WordBtnView(word: word, sentance: self.$sentance)
                                         }
                                     }
-                                }
+                                }.padding(.leading, 15)
+
+                                ScrollView(.horizontal, showsIndicators: true) {
+                                    HStack {
+                                        ForEach(predicates, id: \.id) { word in
+                                            WordBtnView(word: word, sentance: self.$sentance)
+                                        }
+                                    }
+                                }.padding(.leading, 15)
                             }
-                            .frame(width: (geo.size.width - 110) * 0.75 - 80, height: (geo.size.height * 0.8 - 40) / 4)
-                            .background(Color.green)
+                        }
+                        
+                        Spacer()
+                    }
+                    .frame(width: geo.size.width, height: (geo.size.height - 180 - 60) / 3)
+                    
+                    Spacer()
+                    
+                    // 宾语
+                    HStack{
+                        
+                        Spacer()
+                        
+                        ZStack {
+                            
+                            RoundedRectangle(cornerRadius: 6)
+                                .fill(Color.white)
+                                .shadow(radius: 2)
+                            
+                            HStack {
+                                
+                            }
+                            
                             VStack {
-                                HStack {
+                                
+                                HStack(alignment: .firstTextBaseline) {
                                     Text("宾语")
                                         .font(.title2)
                                         .bold()
+                                    Text("  常用词")
+                                        .font(.body)
                                     Spacer()
                                 }
-                                .frame(alignment: .topLeading)
+                                .padding(.leading, 15)
+                                
                                 ScrollView(.horizontal, showsIndicators: true) {
                                     HStack {
                                         ForEach(objects, id: \.id) { word in
                                             WordBtnView(word: word, sentance: self.$sentance)
                                         }
                                     }
-                                }
-                            }
-                            .frame(width: (geo.size.width - 110) * 0.75 - 80, height: (geo.size.height * 0.8 - 40) / 4)
-                            .background(Color.green)
-                            VStack {
-                                HStack {
-                                    Text("常用动作")
-                                        .font(.title2)
-                                        .bold()
-                                    Spacer()
-                                }
-                                .frame(alignment: .topLeading)
+                                }.padding(.leading, 15)
+
                                 ScrollView(.horizontal, showsIndicators: true) {
                                     HStack {
-                                        ForEach(activities, id: \.id) { word in
+                                        ForEach(objects, id: \.id) { word in
                                             WordBtnView(word: word, sentance: self.$sentance)
                                         }
                                     }
+                                }.padding(.leading, 15)
+                                
+                                ScrollView(.horizontal, showsIndicators: true) {
+                                    HStack {
+                                        ForEach(objects, id: \.id) { word in
+                                            WordBtnView(word: word, sentance: self.$sentance)
+                                        }
+                                    }
+                                }.padding(.leading, 15)
+                            }
+                        }
+
+                        Spacer()
+                    }
+                    .frame(width: geo.size.width, height: (geo.size.height - 180 - 60) / 2)
+                    
+                    Spacer()
+                    
+                    // 常用短语
+                    HStack {
+                        VStack {
+                            HStack {
+                                Text("常用短语")
+                                    .font(.title2)
+                                    .bold()
+                                Spacer()
+                            }
+                            ScrollView(.horizontal, showsIndicators: true) {
+                                HStack {
+                                    ForEach(activities, id: \.id) { word in
+                                        WordBtnView(word: word, sentance: self.$sentance)
+                                    }
                                 }
                             }
-                            .frame(width: (geo.size.width - 110) * 0.75 - 80, height: (geo.size.height * 0.8 - 40) / 4)
-                            .background(Color.green)
                         }
-                        .frame(width: (geo.size.width - 110) * 0.75, height: geo.size.height * 0.8)
-                        .background(Color.pink)
-                        
-                        VStack {
-                            Text("推荐词语")
-                        }
-                        .frame(width: (geo.size.width - 110) * 0.25, height: geo.size.height * 0.8, alignment: .center)
-                        .background(Color.gray)
                     }
-                    HStack {
-                        Text(self.sentance)
-                            .font(.largeTitle)
-                            .frame(width: (geo.size.width - 110 - geo.size.height * 0.2 * 0.6 * 2 - 120), height: geo.size.height * 0.2 * 0.6, alignment: .center)
-                            .background(Color.pink)
-                            .padding(10)
-                        Button(action: {
-                            read(text: self.sentance)
-                        }){
-                            Image(systemName: "play.fill")
-                                .font(.system(size: 60, weight: .bold))
-                                .frame(width: geo.size.height * 0.2 * 0.6, height: geo.size.height * 0.2 * 0.6, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        }
-                        .foregroundColor(Color.white)
-                        .background(Color.green)
-                        .cornerRadius(25)
-                        .shadow(color: .gray, radius: 5, x: 2, y: 2)
-                        .padding(10)
-                        Button(action: {
-                            self.sentance = ""
-                        }){
-                            Image(systemName: "clear.fill")
-                                .font(.system(size: 60, weight: .bold))
-                                .frame(width: geo.size.height * 0.2 * 0.6, height: geo.size.height * 0.2 * 0.6, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        }
-                        .foregroundColor(Color.white)
-                        .background(Color.secondary)
-                        .cornerRadius(25)
-                        .shadow(color: .gray, radius: 5, x: 2, y: 2)
-                        .padding(10)
-                    }
-                    .frame(width: geo.size.width - 110, height: geo.size.height * 0.2, alignment: .center)
-                    .background(Color.orange)
+                    .background(Color.green)
+                    .frame(width: geo.size.width - 30, height: (geo.size.height - 180 - 60) / 6)
+                    .shadow(radius: 2)
+                    
+                    Spacer()
                 }
+                .background(Color(red: 249/255, green: 247/255, blue: 243/255).frame(width: geo.size.width, height: geo.size.height - 180))
+                .frame(width: geo.size.width, height: geo.size.height - 180)
             }
         }
     }
--- AphasiaHelper2.0/WordBtnView.swift
+++ AphasiaHelper2.0/WordBtnView.swift
@@ -14,31 +14,66 @@ struct Word {
 
 struct WordBtnView: View {
     
+    var image: UIImage?
     var word: Word
+    
+    @State var isSelected: Bool = false
     
     @Binding var sentance: String
     
     var body: some View {
         Button(action: {
+            isSelected.toggle()
             read(text: "\(word.name)")
             sentance.append("\(word.name) ")
             // TODO: 词频加一
         }){
-            Text("\(word.name)")
-                .frame(width: 90.0, height: 90.0)
-                .font(.title3)
+            if(isSelected) {
+                // 选中
+                VStack {
+                    Image(uiImage: image ?? UIImage(named: "PlaceHolder")!)
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                    Text("\(word.name)")
+                        .foregroundColor(Color.black)
+                        .font(.caption)
+                        .bold()
+                }
+                .frame(width: 60, height: 80, alignment: .center)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 6)
+                        .stroke(Color(red: 61/255, green: 141/255, blue: 136/255), lineWidth: 8)
+                )
+                .background(Color.white)
+                .cornerRadius(6)
+            }
+            else {
+                // 未选中
+                VStack {
+                    Image(uiImage: image ?? UIImage(named: "PlaceHolder")!)
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                    Text("\(word.name)")
+                        .foregroundColor(Color.black)
+                        .font(.caption)
+                        .bold()
+                }
+                .frame(width: 60, height: 80, alignment: .center)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 6)
+                        .stroke(Color.gray.opacity(0.3), lineWidth: 2)
+                        .shadow(color: Color.gray, radius: 2)
+                )
+                .background(Color.white)
+                .cornerRadius(6)
+            }
         }
-        .foregroundColor(Color.white)
-        .background(Color.blue)
-        .cornerRadius(20)
-        .shadow(color: .gray, radius: 3, x: 2, y: 2)
-        .padding(6)
     }
 }
 
 //struct WordBtnView_Previews: PreviewProvider {
 //    static var previews: some View {
 //
-//        WordBtnView(word: Word(id: 0, name: ""))
+//        WordBtnView(word: Word(id: 0, name: "null"))
 //    }
 //}
